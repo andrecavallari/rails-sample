1a87ee59caf7323f857535cdef158ad2ec6470d8
Andre <cavallari@live.com>
2021-08-15 12:06:26 -0300

    Spec open weather api

--- spec/lib/open_weather_api_spec.rb
+++ spec/lib/open_weather_api_spec.rb
@@ -1,19 +1,49 @@
 # frozen_string_literal: true
 
-RSpec.describe OpenWeatherApi, :vcr => { record: :new_episodes } do
+RSpec.describe OpenWeatherApi, :vcr do
   describe '.current_weather' do
-    subject(:action) { described_class.get_weather(city, 'PR', 'BR') }
+    subject(:action) { described_class.current_weather(city, 'PR', 'BR') }
 
     context 'with correct city name' do
       let(:city) { 'Cascavel' }
+      let(:expected_json) { JSON.parse file_fixture('open_weather/weather_response.json').read }
 
-      it do
-        expect(action).to eq('')
+      it 'retrieve city current weather data', :aggregate_failures do
+        expect(JSON.parse(action.body)).to eq(expected_json)
+        expect(action.code).to eq(200)
       end
     end
 
     context 'when wrong city name' do
       let(:city) { 'Asdf' }
+
+      it 'returns error', :aggregate_failures do
+        expect(action.body).to eq('{"cod":"404","message":"city not found"}')
+        expect(action.code).to eq(404)
+      end
+    end
+  end
+
+  describe '.forecast' do
+    subject(:action) { described_class.forecast(city, 'PR', 'BR') }
+
+    context 'with correct city name' do
+      let(:city) { 'Cascavel' }
+      let(:expected_json) { JSON.parse file_fixture('open_weather/forecast_response.json').read }
+
+      it 'retrieve city current weather data', :aggregate_failures do
+        expect(JSON.parse(action.body)).to eq(expected_json)
+        expect(action.code).to eq(200)
+      end
+    end
+
+    context 'when wrong city name' do
+      let(:city) { 'Asdf' }
+
+      it 'returns error', :aggregate_failures do
+        expect(action.body).to eq('{"cod":"404","message":"city not found"}')
+        expect(action.code).to eq(404)
+      end
     end
   end
 end
