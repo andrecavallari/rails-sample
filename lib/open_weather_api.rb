a45d4902189ddfb27d1bf13cc6e07277fecb8fdd
André <cavallari@live.com>
2021-08-15 12:06:59 -0300

    Feature: Openweather api (#2)
    * Add vscode to gitignore
    
    * Add some gems
    
    * Configure rubocop
    
    * Setup application
    
    * Autoload open weather api
    
    * Create open weather api
    
    * Add vcr
    
    * Spec open weather api
    
    * Add new rules to rubocop
    
    * Add superdiff gem
    
    * require gem
    
    * Spec open weather api

---
+++ lib/open_weather_api.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class OpenWeatherApi
+  include HTTParty
+  base_uri 'api.openweathermap.org/data/2.5'
+  DEFAULT_UNITS = 'metric'
+  DEFAULT_LANGUAGE = 'pt_br'
+  DEFAULT_COUNTRY = 'BR'
+
+  class << self
+    def current_weather(city, state = nil, country = DEFAULT_COUNTRY)
+      query_string = [city, state, country].compact.join(',')
+      get('/weather', query: { q: query_string })
+    end
+
+    def forecast(city, state = nil, country = DEFAULT_COUNTRY)
+      query_string = [city, state, country].compact.join(',')
+      get('/forecast', query: { q: query_string })
+    end
+
+    private
+
+    def get(path, query: {})
+      super(path, query: query.merge(
+        appid: ENV['OPEN_WEATHER_API_KEY'],
+        units: DEFAULT_UNITS,
+        lang: DEFAULT_LANGUAGE
+      ))
+    end
+  end
+end
