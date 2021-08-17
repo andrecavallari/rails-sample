28cac99b216b173bad5dbb352ed9632ea50e48f7
Andre <cavallari@live.com>
2021-08-17 19:49:33 -0300

    Create directories controller

--- app/controllers/filesystem/directories_controller.rb
+++ app/controllers/filesystem/directories_controller.rb
@@ -2,5 +2,57 @@
 
 module Filesystem
   class DirectoriesController < ApplicationController
+    include JSONAPI::Deserialization
+    include JSONAPI::Fetching
+
+    before_action :set_directory, only: %i[show update destroy]
+
+    def index
+      render jsonapi: Directory.where(parent_id: nil)
+    end
+
+    def show
+      render jsonapi: @directory, children: Directory.all
+    end
+
+    def create
+      directory = Directory.new(directory_params)
+
+      if directory.save
+        render jsonapi: directory
+      else
+        render jsonapi_errors: directory.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @directory.update(directory_params)
+        render jsonapi: @directory
+      else
+        render jsonapi_errors: @directory.errors
+      end
+    end
+
+    def destroy
+      if @directory.destroy
+        head :no_content
+      else
+        render jsonapi_errors: @directory.errors
+      end
+    end
+
+    private
+
+    def directory_params
+      jsonapi_deserialize(params, only: %i[name parent_id])
+    end
+
+    def set_directory
+      @directory = Directory.find(params[:id])
+    end
+
+    def jsonapi_meta(resources)
+      { count: resources.count } if resources.respond_to?(:count)
+    end
   end
 end
