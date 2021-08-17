911bc53601f1f65496effc2de0def8ae3e2dada3
Andre <cavallari@live.com>
2021-08-17 19:49:10 -0300

    Create directory serializer

---
+++ app/serializers/filesystem/directory_serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Filesystem
+  class DirectorySerializer
+    include JSONAPI::Serializer
+
+    attributes :id, :name, :created_at, :updated_at
+    belongs_to :parent, record_type: :directory, serializer: Filesystem::DirectorySerializer
+    has_many :children, record_type: :directory, serializer: Filesystem::DirectorySerializer
+  end
+end
