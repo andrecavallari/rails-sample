098596972283764e398cb0c32732df91a26d67eb
André <cavallari@live.com>
2021-08-21 19:34:08 -0300

    Feat: Rubocop config (#8)
    * Apply rubocop autocorrect
    
    * Fix uniqueness without index
    
    * Apply rubocop changes
    
    * Config rubocop on gh actions
    
    * Try bin/rubocop
    
    * Removed ruby-
    
    * Try ruby 3.0
    
    * Back to old version of ruby version
    
    * merge: excluded

--- db/migrate/20210821180832_create_store_segments.rb
+++ db/migrate/20210821180832_create_store_segments.rb
@@ -2,7 +2,7 @@ class CreateStoreSegments < ActiveRecord::Migration[6.1]
   def change
     create_table :store_segments do |t|
       t.timestamps
-      t.string :name, null: false
+      t.string :name, null: false, index: { unique: true }
       t.string :operation, null: false
     end
   end
