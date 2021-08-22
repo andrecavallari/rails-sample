f1aaf521894be700ecd87ce6b9779f2ec347fdee
André <cavallari@live.com>
2021-08-22 14:52:07 -0300

    Feat: Undercover (#12)
    * Update ruby to 3.0.2 and add gems
    
    * Update dockerfile ruby version
    
    * Ignore coverage directory
    
    * Add simplecov to rspec
    
    * Add cmake installation for rugged in Dockerfile
    
    * Update ruby version
    
    * Update rubocop to run with bundle exec
    
    * Add undercover
    
    * Update description
    
    * Update description

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM ruby:3.0.1
+FROM ruby:3.0.2
 
 WORKDIR /app
 COPY Gemfile Gemfile.lock /app/
 
+RUN apt-get update && apt-get install -y cmake
 RUN gem install bundler \
   && bundle install --no-cache --jobs=4 --retry=3
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start do
+  add_filter(%r{^/spec/})
+  enable_coverage(:branch)
+end
+
+require 'undercover'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
