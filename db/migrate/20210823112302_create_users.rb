28de18eb1645cb971669a50b78866bffa28dd69e
Andre <cavallari@live.com>
2021-08-23 18:58:20 -0300

    Create user model

---
+++ db/migrate/20210823112302_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.timestamps
+      t.string :name, null: false
+      t.string :email, null: false, index: { unique: true }
+      t.string :password, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_184831) do
+ActiveRecord::Schema.define(version: 2021_08_23_112302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2021_08_21_184831) do
     t.string "name", null: false
     t.string "operation", null: false
     t.index ["name"], name: "index_store_segments_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
