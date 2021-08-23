61514845e9ccd8cf81ddae4171bf8938c05fd87c
Andre <cavallari@live.com>
2021-08-23 18:59:52 -0300

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
+  namespace :auth do
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    get '/sessions', to: 'sessions#index'
+    delete '/revoke/:jti', to: 'sessions#revoke'
+  end
+
   resources :tweet_weather, only: %i[create]
 
   namespace :filesystem do
