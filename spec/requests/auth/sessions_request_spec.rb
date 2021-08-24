f39712637f1c1b50e3f7b37d80d49459e65c9bfa
Andre <cavallari@live.com>
2021-08-24 00:27:16 -0300

    Spec sessions

--- spec/requests/auth/sessions_request_spec.rb
+++ spec/requests/auth/sessions_request_spec.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Auth Requests', type: :request do
-  let(:json_response) { JSON.parse(response.body, symbolize_names: true) }
+  include Helpers
 
-  describe '/auth/login' do
+  let(:json_response) { JSON.parse(response.body, symbolize_names: true) }
+  let(:user) { create(:user, email: 'lorem@ipsum.com', password: 'loremipsum') }
+
+  before { JWT::DB.clear }
+
+  after { JWT::DB.clear }
+
+  describe 'GET /auth/sessions' do
+    let(:user) { create(:user) }
+
+    before do
+      2.times { JWT::DB.create(user.id, { user_agent: 'Mozilla' }) }
+      get '/auth/sessions', headers: auth_header(user)
+    end
+
+    it 'returns a 200 status code', :aggregate_failures do
+      expect(response).to have_http_status(:ok)
+      expect(json_response.size).to eq(3)
+    end
+  end
+
+  describe 'POST /auth/login' do
     subject(:do_request) { post '/auth/login', params: { email: email, password: password } }
 
-    let(:user) { create(:user, email: 'lorem@ipsum.com', password: 'loremipsum') }
     let(:email) { user.email }
     let(:password) { 'loremipsum' }
 
@@ -33,6 +53,35 @@ RSpec.describe 'Auth Requests', type: :request do
       it 'returns unauthorized' do
         expect(response).to have_http_status(:unauthorized)
       end
+    end
+  end
+
+  describe 'DELETE /auth/logout' do
+    context 'with auth header' do
+      before { delete '/auth/logout', headers: auth_header(user) }
+
+      it 'returns ok', :aggregate_failures do
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'without auth header' do
+      before { delete '/auth/logout' }
+
+      it 'returns unprocessable entity' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'DELETE /auth/revoke' do
+    subject(:do_request) { delete auth_path(jti), headers: headers }
+
+    let!(:headers) { auth_header(user) }
+    let!(:jti) { JWT::DB.create(user.id, { user_agent: 'Webkit' }) }
+
+    it 'excludes a key' do
+      expect { do_request }.to change { JWT::DB.user_keys(user.id).size }.by(-1)
     end
   end
 end
