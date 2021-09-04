0884ddf545d88b9fccdac446c3b81229ee1dc16c
Andre <cavallari@live.com>
2021-09-04 19:47:09 -0300

    Include time helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,6 +19,8 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include ActiveSupport::Testing::TimeHelpers
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
