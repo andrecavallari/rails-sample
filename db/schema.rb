9c98c2a7fe30f99297289e011a0eeecef676f535
Andre <cavallari@live.com>
2021-09-04 19:46:33 -0300

    Use default rails authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_112302) do
+ActiveRecord::Schema.define(version: 2021_09_04_172820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_112302) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
