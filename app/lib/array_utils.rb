28ac5ac30f740e6039728619ebee43d3e8fda585
Andre <cavallari@live.com>
2021-12-22 19:37:32 -0300

    Fix rubocop complain about param size

--- app/lib/array_utils.rb
+++ app/lib/array_utils.rb
@@ -2,8 +2,8 @@
 
 class ArrayUtils
   class << self
-    def symetric_difference(a, b)
-      (a - b) + (b - a)
+    def symetric_difference(array_a, array_b)
+      (array_a - array_b) + (array_b - array_a)
     end
   end
 end
