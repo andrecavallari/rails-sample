780f5c7014b49e2caa6152e6a1efdd325c677512
Andre <cavallari@live.com>
2022-09-13 09:12:06 -0300

    Improve MQ client

--- app/lib/mq/client.rb
+++ app/lib/mq/client.rb
@@ -2,19 +2,15 @@
 
 module Mq
   class Client
+    CLOUDAMQP_URL = ENV.fetch('CLOUDAMQP_URL', 'amqp://localhost:5672')
+
     class << self
       def connection
-        @connection ||= Bunny.new(connection_string).tap(&:start)
+        @connection ||= Bunny.new(CLOUDAMQP_URL).tap(&:start)
       end
 
       def channel
         @channel ||= connection.create_channel
-      end
-
-      private
-
-      def connection_string
-        @connection_string ||= ENV.fetch('CLOUDAMQP_URL', 'amqp://localhost:5672')
       end
     end
   end
